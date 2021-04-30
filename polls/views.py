b8b02fb07092916d52a2457f524cd7d2b9331397
iMac21.5-2012 <61crossroad@gmail.com>
2021-05-01 01:11:12 +0900

    post request to persist, F()

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from .models import Question
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
@@ -27,9 +30,30 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+    # response = "You're looking at the results of question %s."
+    # return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        # selected_choice.votes += 1
+        # avoiding race condition using F()
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # call refresh_from_db when retrieve again
+        selected_choice.refresh_from_db()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+    # return HttpResponse("You're voting on question %s." % question_id)
